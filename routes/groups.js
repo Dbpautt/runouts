d65b23e027b713de27f0483d4ded03b94834f825
Edu Benito <eduardbenito@icloud.com>
2018-07-30 20:32:14 +0200

    users dromdowns filled with username in group list

--- routes/groups.js
+++ routes/groups.js
@@ -2,12 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const Group = require('../models/group');
+const User = require('../models/user');
 
 /* GET groups */
 router.get('/', (req, res, next) => {
   Group.find()
     .then(groups => {
-      console.log(groups);
+      groups.forEach((group) => {
+        User.find({ _id : { $in : group.members } })
+          .then(users => {
+            group.members = users;
+          })
+      })
+      
       res.render('groups', { groups });
     })
     .catch(error => {
