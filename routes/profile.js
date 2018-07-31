0c677d9b1493d06c89f907b62f1463896f7abd09
Edu Benito <eduardbenito@icloud.com>
2018-07-31 10:11:20 +0200

    remove generic profile get route

--- routes/profile.js
+++ routes/profile.js
@@ -3,10 +3,6 @@ const router = express.Router();
 
 const User = require('../models/user');
 
-router.get('/', (req, res, next) => {   // És necessari? Podem fer servir sempre /:id????? Revisar rutes a readme
-  res.send('profile page');
-});
-
 router.get('/:id', (req, res, next) => {
   const { id } = req.params;
   User.findById(id)
