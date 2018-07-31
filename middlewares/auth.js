40e7bb5646866ad278ed0b77acff4183ae7f2c4c
SandraND <lainfaith2@gmail.com>
2018-07-31 17:59:06 +0200

    required users

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,20 @@
-
 const requireUser = (req, res, next) => {
-    if (req.session.currentUser) {
-      next();
-    } else {
-      res.redirect('/auth/login');
-    }
-  };
-  
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect('/auth/login');
+  }
+};
+
+const requireAnon = (req, res, next) => {
+  if (!req.session.currentUser) {
+    next();
+  } else {
+    res.redirect('/books');
+  }
+};
+
 module.exports = {
-  requireUser
+  requireUser,
+  requireAnon
 };
