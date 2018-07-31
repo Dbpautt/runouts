28c589bbc325bb2cd9e28668dc6939126299f4de
SandraND <lainfaith2@gmail.com>
2018-07-31 12:14:04 +0200

    user list

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
+const User = require('../models/user');
+const Group = require('../models/group');
+
+/* GET users */
 router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+  User.find()
+    .then(users => {
+      users.forEach((group) => {
+        Group.find({ members : users._id })
+          .then(groups => {
+            group.name = groups;
+          })
+      })
+      
+      res.render('users', { users });
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  User.findById(id)
+    .then(user => {
+      res.render('user_detail', user);
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 module.exports = router;
+
